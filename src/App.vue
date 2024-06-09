827045119dab67b93d0f46674e7e1db8a35868c7
Sunil Bagde <bagdesunil786@gmail.com>
2024-06-10 03:03:49 +0530

    feat: init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+import Hero from './components/Hero.vue'
+
+</script>
+
+<template>
+  <Hero />
+
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/tool/unplugin/src/index.ts
@@ -0,0 +1,17 @@
+import { createUnplugin } from 'unplugin'
+import VitePluginInspector from '.'
+import type { Options } from './types'
+
+export default createUnplugin<Options, true>((options) => {
+  const plugins = VitePluginInspector(options) as any
+  return [
+    {
+      name: 'unplugin-vue-inspector',
+      vite: plugins[0],
+    },
+    {
+      name: 'unplugin-vue-inspector:post',
+      vite: plugins[1],
+    },
+  ]
+})
